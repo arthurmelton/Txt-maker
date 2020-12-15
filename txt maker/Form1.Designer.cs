143dd02693a84b69ced76f7e252363a150284e8f
AMTitan <amtitan@icloud.com>
2020-12-15 09:37:29 -0500

    Add files via upload

---
+++ txt maker/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace txt_maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(211, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How many numbers? :";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(103, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "List out numbers";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(13, 66);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(124, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "List out odd numbers";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(13, 89);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(130, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "List out even numbers";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.CheckBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(13, 112);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(164, 17);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "List out square roots numbers";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.CheckBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(13, 135);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(131, 17);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "List out prime numbers";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.CheckBox5_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Starting numeber :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(502, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(694, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Negitive";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 11;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(13, 158);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(152, 17);
+            this.checkBox6.TabIndex = 12;
+            this.checkBox6.Text = "List out non prime numbers";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.CheckBox6_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
---
+++ txt maker/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace txt_maker
+{
+    public partial class Form1 : Form
+    {
+
+        string Location1 = Application.StartupPath + "\\numbers.txt";
+        List<string> lines = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if(!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Please write a number");
+            }
+
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Please write a number");
+            }
+
+            if (textBox1.Text != "" && checkBox1.Checked == true && textBox2.Text != "")
+            {
+                int howManyTimes = int.Parse(textBox1.Text) + int.Parse(textBox2.Text) + 1;
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                for (int i = int.Parse(textBox2.Text) + 1; i < howManyTimes ; i++)
+                {
+                    lines.Add(i.ToString());
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+            if (textBox1.Text != "" && checkBox2.Checked == true && textBox2.Text != "")
+            {
+                int howManyTimes = (int.Parse(textBox1.Text) * 2) + int.Parse(textBox2.Text);
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                for (int i = int.Parse(textBox2.Text) + 1; i < howManyTimes; i+=2)
+                {
+                    if (i % 2 == 0)
+                    {
+                        i++;
+                        howManyTimes++;
+                    }
+
+                    lines.Add(i.ToString());
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+            if (textBox1.Text != "" && checkBox3.Checked == true && textBox2.Text != "")
+            {
+                int howManyTimes = (int.Parse(textBox1.Text) * 2) + int.Parse(textBox2.Text);
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                for (int i = int.Parse(textBox2.Text) + 1; i < howManyTimes; i += 2)
+                {
+                    if(Math.Abs(i)%2 == 1)
+                    {
+                        i++;
+                        howManyTimes++;
+                    }
+
+                    lines.Add(i.ToString());
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+
+            if (textBox1.Text != "" && checkBox4.Checked == true && textBox2.Text != "")
+            {
+                int howManyTimes = int.Parse(textBox1.Text) + int.Parse(textBox2.Text) + 1;
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                for (int i = int.Parse(textBox2.Text) + 1; i < howManyTimes; i++)
+                {
+                    if(i < 0)
+                    {
+                        i = 1;
+
+                        howManyTimes += Math.Abs(int.Parse(textBox2.Text));
+
+                    }
+                    lines.Add((i*i).ToString());
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+            if (textBox1.Text != "" && checkBox5.Checked == true && textBox2.Text != "")
+            {
+
+                int howManyTimes = int.Parse(textBox1.Text) + 1;
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                int i = 1;
+
+                for (int b = int.Parse(textBox2.Text) + 1; i < howManyTimes; b++)
+                {
+                    int a = 0;
+                    for (int x = 1; x <= b; x++)
+                    {
+                        if (b % x == 0)
+                        {
+                            a++;
+                        }
+                    }
+
+                    if(a==2)
+                    {
+                        lines.Add(b.ToString());
+                        i++;
+                    }
+
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+
+            if (textBox1.Text != "" && checkBox6.Checked == true && textBox2.Text != "")
+            {
+
+                int howManyTimes = int.Parse(textBox1.Text) + 1;
+
+                if (File.Exists(Location1))
+                {
+                    File.Delete(Location1);
+                }
+
+                lines.Clear();
+
+                int i = 1;
+
+                for (int b = int.Parse(textBox2.Text) + 1; i < howManyTimes; b++)
+                {
+                    int a = 0;
+                    for (int x = 1; x <= b; x++)
+                    {
+                        if (b % x == 0)
+                        {
+                            a++;
+                        }
+                    }
+
+                    if (a != 2)
+                    {
+                        lines.Add(b.ToString());
+                        i++;
+                    }
+
+                }
+
+                File.WriteAllLines(Location1, lines);
+
+                MessageBox.Show("Created File!");
+            }
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked == true || checkBox3.Checked == true || checkBox4.Checked == true || checkBox5.Checked == true || checkBox6.Checked == true)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox3.Checked == true || checkBox4.Checked == true || checkBox5.Checked == true || checkBox6.Checked == true)
+            {
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void CheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox4.Checked == true || checkBox5.Checked == true || checkBox6.Checked == true)
+            {
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void CheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox3.Checked == true || checkBox5.Checked == true || checkBox6.Checked == true)
+            {
+                checkBox4.Checked = false;
+            }
+        }
+
+        private void CheckBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox3.Checked == true || checkBox4.Checked == true || checkBox6.Checked == true)
+            {
+                checkBox5.Checked = false;
+            }
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void CheckBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox3.Checked == true || checkBox4.Checked == true || checkBox5.Checked == true)
+            {
+                checkBox6.Checked = false;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(!textBox2.Text.Contains("-"))
+            {
+                textBox2.Text = "-" + textBox2.Text;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 189)
+            {
+                if (!textBox2.Text.Contains("-"))
+                {
+                    textBox2.Text = "-" + textBox2.Text;
+                }
+            }
+        }
+    }
+}
